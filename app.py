9e02513c40ac0f151cc88cf49acd697a91f75115
kkeck <kkeck1@binghamton.edu>
2018-09-21 15:36:35 -0400

    Testing

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 server = app.server
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+app.css.append_css(
+    {"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2(
+    'Who\'s better? Karissa or Kevin?\nPlease select your answer from below'
+    ),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        options=[{'label': i, 'value': i} for i in ['Kevin', 'Kevin']],
+        value='Select an answer'
     ),
     html.Div(id='display-value')
 ])
