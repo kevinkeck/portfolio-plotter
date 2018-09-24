5532457dac5349949f68604b665ab61a3afe4eef
kkeck <kkeck1@binghamton.edu>
2018-09-24 19:11:31 -0400

    Web app actually functions, it can display multiple candlestick charts

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import os
+import datetime as dt
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
+import pandas as pd
 import pandas_datareader as pdr
-from pandas_datareader.data import get_quote_yahoo
-
-data = get_quote_yahoo('AMZN')
-price = (data['price'])
 
 
 app = dash.Dash(__name__)
@@ -18,26 +17,64 @@ app.css.append_css(
     {"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
+    html.H1('Portfolio Plotter'),
     html.H2(
-    #'Select tickers from dropdown below'
-    str(price)
+    'Select tickers from dropdown below'
     ),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['AMZN', 'SQUARE', 'LMT', 
-        'AAPL', 'MJ', 'CGC', 'ACBFF', 'NFLX', 'TESLA', 'ZBRA', 'MU']],
+        options=[{'label': i, 'value': i} for i in ['AMZN', 'SQ', 'LMT', 
+        'AAPL', 'MJ', 'CGC', 'NFLX', 'TSLA', 'ZBRA', 'MU']],
         multi=True,
-        value=['AMZN', 'SQUARE', 'LMT', 
-        'AAPL', 'MJ', 'CGC', 'ACBFF', 'NFLX', 'TESLA', 'ZBRA', 'MU']
-    ),
-    html.Div(id='display-value')
-])
+        value=['AMZN']
+    ),        
+    html.Div([html.H3('Enter start & end dates:'),
+    dcc.DatePickerRange(id='date_picker',
+                        min_date_allowed = dt.datetime(2015,1,1),
+                        max_date_allowed = dt.datetime.today(),
+                        start_date = dt.datetime(2018, 9, 1),
+                        end_date = dt.datetime.today()
+					)
+				], style={'display':'inline-block'}),
+    html.Div(id='display_value')
+    ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-# display the stock prices for all selected values 
+
+@app.callback(dash.dependencies.Output('display_value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value'),
+              dash.dependencies.Input('date_picker', 'start_date'),
+              dash.dependencies.Input('date_picker', 'end_date')])
+def make_dataframe(ticker_list, start_date, end_date):
+    # I need to change this to make multiple dataframes. 1 for each ticker.
+    graph_list = []
+    for ticker in ticker_list:
+        start= start_date
+        end= end_date
+        #end = dt.datetime.today()
+        #start = dt.datetime(2018,1,1)
+        # Needed to reset_index to make date back into a column header
+        # Now this is indexed by an id number
+        df = pdr.DataReader(ticker.upper(), 'iex', start, end).reset_index()
+        graph_list.append(dcc.Graph(
+            id=ticker,
+            figure={
+                'data':[
+                    go.Candlestick(
+                        x=df.date,
+                        open=df.open,
+                        high=df.high,
+                        low=df.low,
+                        close=df.close,
+                        increasing=dict(line=dict(color= '#17BECF')),
+                        decreasing=dict(line=dict(color= '#7F7F7F')))
+                        ]
+                        }
+        ))
+    print(graph_list)
+    return(graph_list)
+        # I could make a graph using the same function
+        # updated graphs should replace old graphs. 
+        # and when a ticker is removed, the graph should be removed    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
